e9f945655080a6abaacbd5da54e29a354f4dd429
bmeredith <1832575+bmeredith@users.noreply.github.com>
2023-11-20 14:40:34 -0600

    initial checkin

---
+++ test/TBD.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {TBD} from "../src/TBD.sol";
+
+contract TBDTest is Test {
+    TBD public tbd;
+
+    function setUp() public {
+        tbd = new TBD();
+    }
+}
