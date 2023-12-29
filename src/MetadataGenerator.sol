ef0db9caaf15e79bf275ae3628a49dda6d9d320a
bmeredith <1832575+bmeredith@users.noreply.github.com>
2023-12-28 22:09:11 -0600

    added logic to output metadata attributes metadata is converted to base64

--- src/MetadataGenerator.sol
+++ src/MetadataGenerator.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.8.23;
 
 import {ITokenDescriptor} from "./ITokenDescriptor.sol";
 import {JsonWriter} from "solidity-json-writer/JsonWriter.sol";
+import {Base64} from '@openzeppelin/contracts/utils/Base64.sol';
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MetadataGenerator {
     using JsonWriter for JsonWriter.Json;
 
-    function generateMetadata(uint256 tokenId, ITokenDescriptor.Token calldata token) external view returns (string memory) {
+    function generateMetadata(uint256 tokenId, ITokenDescriptor.Token calldata token) 
+        external 
+        pure 
+        returns (string memory) 
+    {
         JsonWriter.Json memory writer;
         writer = writer.writeStartObject();
 
@@ -33,6 +38,44 @@ contract MetadataGenerator {
             string.concat('arweave://')
         );
 
+        writer = _generateAttributes(writer);
+
+        writer = writer.writeEndObject();
+
+        return string(
+            abi.encodePacked(
+                'data:application/json;base64,',
+                Base64.encode(abi.encodePacked(writer.value))
+            )
+        );
+    }
+
+    function _determineImage(ITokenDescriptor.Coordinate calldata coordinate) 
+        private
+        pure
+        returns (uint256) 
+    {
+
+    }
+
+    function _generateAttributes(JsonWriter.Json memory _writer) 
+        private 
+        pure 
+        returns (JsonWriter.Json memory writer)
+    {
+        writer = _writer.writeStartArray('attributes');
+
+        writer = writer.writeEndArray();
+    }
+
+    function _addStringAttribute(
+        JsonWriter.Json memory _writer,
+        string memory key,
+        string memory value
+    ) private pure returns (JsonWriter.Json memory writer) {
+        writer = _writer.writeStartObject();
+        writer = writer.writeStringProperty('trait_type', key);
+        writer = writer.writeStringProperty('value', value);
         writer = writer.writeEndObject();
     }
 }
