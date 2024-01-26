0556a862042f5949635b2578fb1fd35611aaf78c
bmeredith <1832575+bmeredith@users.noreply.github.com>
2024-01-26 01:06:07 -0600

    removed unneeded deploy script

--- script/TBD.s.sol
+++ script/TBD.s.sol
@@ -1,12 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity =0.8.23;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Descriptor} from '../src/Descriptor.sol';
+import {TBD} from '../src/TBD.sol';
+import {Script} from 'forge-std/Script.sol';
 
-contract TBDScript is Script {
-    function setUp() public {}
+abstract contract Deploy is Script {
+  function _deploy() internal {
+    vm.startBroadcast();
+    Descriptor descriptor = new Descriptor();
+    new TBD(address(descriptor));
+    vm.stopBroadcast();
+  }
+}
 
-    function run() public {
-        vm.broadcast();
-    }
+contract DeployMainnet is Deploy {
+  function run() external {
+    _deploy();
+  }
+}
+
+contract DeploySepolia is Deploy {
+  function run() external {
+    _deploy();
+  }
 }
