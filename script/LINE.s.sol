ce94b8d08435a910dea83ccf0ac25c7cba7ae5f8
bmeredith <1832575+bmeredith@users.noreply.github.com>
2024-01-30 15:32:46 -0600

    in LINE script, added (dummy) coordinates to be set as mintable when deployed out

--- script/LINE.s.sol
+++ script/LINE.s.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.23;
 
+import {ITokenDescriptor} from "../src/ITokenDescriptor.sol";
 import {Descriptor} from '../src/Descriptor.sol';
 import {LINE} from '../src/LINE.sol';
 import {Script} from 'forge-std/Script.sol';
@@ -9,8 +10,23 @@ abstract contract Deploy is Script {
   function _deploy() internal {
     vm.startBroadcast();
     Descriptor descriptor = new Descriptor();
-    new LINE(address(descriptor));
+    LINE line = new LINE(address(descriptor));
+
+    line.setInitialAvailableCoordinates(_getMintableCoordinates());
     vm.stopBroadcast();
+  }
+
+  function _getMintableCoordinates() private pure returns (ITokenDescriptor.Coordinate[] memory) {
+    ITokenDescriptor.Coordinate[] memory coordinates = new ITokenDescriptor.Coordinate[](20);
+    for(uint256 i=0;i < 10;i++) {
+        coordinates[i] = (ITokenDescriptor.Coordinate({x: i+1, y: 1}));
+    }
+
+    for(uint256 i=0;i < 10;i++) {
+        coordinates[i+10] = (ITokenDescriptor.Coordinate({x: i+1, y: 24}));
+    }
+
+    return coordinates;
   }
 }
 
